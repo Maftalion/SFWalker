16e81117c0102714b042fa9b37d1bf40dcc10d22
Vincent Huang <vincent.vwh.huang@gmail.com>
2016-08-12 10:32:56 -0700

    Server pathcolor refactor

--- server/server.js
+++ server/server.js
@@ -1,27 +1,32 @@
 var express = require('express');
+var parser = require('body-parser');
 var app = express();
 
-//transforms nodes from interhashScores.json into array of objects expected by MapBox annotations, in order to draw street colors 
 var generatePathColors = require('./pathColors');
+
+module.exports = app;
+
+app.use(parser.json());
+
+//transforms nodes from interhashScores.json into array of objects expected by MapBox annotations, in order to draw street colors 
 
 var port = 3000;
 
-generatePathColors
-.then(function(result){
-
-	app.get('/allstreets', function(req, res) {
-		res.type('application/json');
-		res.send(result);
-	});
-
-	app.listen(port);
-	console.log('Listening on port ' + port);
-
-})
-.catch(function(e) {
-    console.log(e);
+app.get('/allstreets', function(req, res) {
+  generatePathColors
+  .then(function(result){
+    res.type('application/json');
+    res.status(200);
+    res.send(result);
+  })
+  .catch(function(e) {
+      console.log(e);
+      res.sendStatus(500);
+  });
 });
 
-
+app.post('/routes', function(req, res) {
+  
+})
 
 
