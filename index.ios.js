93b5db35172c12c5d1dd853c9ce633ad44831aa4
Matt <aftalion.matt@gmail.com>
2016-08-12 10:40:41 -0700

    ignore build file

--- index.ios.js
+++ index.ios.js
@@ -24,52 +24,54 @@ class MapExample extends Component {
     center: {
       latitude: 37.7836925,
       longitude: -122.4111781
-      
+
     },
     zoom: 13,
     userTrackingMode: Mapbox.userTrackingMode.follow,
-    annotations: [{
-      coordinates: [[37.8038329212967,-122.40498607552468],[37.80402008919458,-122.40345922804256],[37.80403232677321,-122.40335872902352],[37.804043223247334,-122.40326745009796],[37.8041535290931, -122.40238315931316],[37.80418009972615, -122.40232004358228],[37.80419669589443, -122.4022807324564]],
-      type: 'polyline',
-      strokeColor: '#00FB00',
-      strokeWidth: 3,
-      id: 'navigateLine'
-    }, {
-      coordinates: [37.8038329212967, -122.40498607552468],
-      id: 'Start',
-      type: 'point',
-      title: 'Starting Location',
-      annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-        height: 15,
-        width: 15
-      }
-    }, {
-      coordinates: [37.80419669589443, -122.4022807324564],
-      id: 'End',
-      type: 'point',
-      title: 'Destination',
-      annotationImage: {
-        source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
-        height: 15,
-        width: 15
-      }
-    }]
+    annotations: []
+  };
 
   };
-  //In progress
-  fixCoords = (coordinates) => {
-    this.setState({
-      coordinates: this.state.coordinates.map(function(item) {
-      [item[0],item[1]] = [item[1], item[0]];
-      })
-    });
+  handleStart = (input) => {
+    convertGPS(input)
+      .then((data) => {
+        this.setState({
+          annotations: this.state.annotations.concat([{
+            coordinates: [data.lat, data.lon],
+            title: data.name,
+            type: 'point',
+            annotationImage: {
+              source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
+              height: 25,
+              width: 25
+            },
+            id: 'entered location'
+          }])
+        })
+        this._map.setCenterCoordinateZoomLevel(data.lat, data.lon, 15, true);
+      });
   };
-  //In progress
-  setCoords = (input, state) => {
-    var coords = convertGPS(input);
-  }
 
+  handleDest = (input) => {
+    convertGPS(input)
+      .then((data) => {
+        this.setState({
+          annotations: this.state.annotations.concat([{
+            coordinates: [data.lat, data.lon],
+            title: data.name,
+            type: 'point',
+            annotationImage: {
+              source: { uri: 'https://cldup.com/7NLZklp8zS.png' },
+              height: 25,
+              width: 25
+            },
+            id: 'entered destination'
+          }])
+        })
+        this._map.setCenterCoordinateZoomLevel(data.lat, data.lon, 15, true);
+      });
+  };
+  
   onRegionDidChange = (location) => {
     this.setState({ currentZoom: location.zoomLevel });
     console.log('onRegionDidChange', location);
@@ -98,7 +100,7 @@ class MapExample extends Component {
     fetch('http://localhost:3000/allstreets')
       .then((response) => response.json())
       .then((responseJson) => {
-        
+
         mainComponent.setState({ annotations: responseJson });
 
         //return responseJson;
